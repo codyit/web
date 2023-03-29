7da91f775db2a51ed844c56ddc3d1ca6392bcc2e
Cody T.-H. Chiu <codyit@protonmail.com>
2023-03-29 22:25:27 +0800

    Small clean up

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import useConfig from "./components/useConfig";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import MenuIcon from "@mui/icons-material/Menu";
-import { AppBar, Box, Container, createTheme, IconButton, ThemeProvider, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, createTheme, IconButton, ThemeProvider, Toolbar, Typography } from "@mui/material";
 
 /**
  * Our Web Application
@@ -23,6 +23,24 @@ export default function App() {
       },
     },
   });
+
+  const SocialIcons = () => (
+    <Box>
+      <IconButton
+        edge="start"
+        color="inherit"
+        aria-label="linkedin"
+        href="https://www.linkedin.com/in/cody-t-h-c-647ab962"
+        target="_blank"
+      >
+        <LinkedInIcon />
+      </IconButton>
+      <IconButton edge="end" color="inherit" aria-label="github" href="https://github.com/codyit" target="_blank">
+        <GitHubIcon />
+      </IconButton>
+    </Box>
+  );
+
   return (
     <ThemeProvider theme={theme}>
       <Box sx={{ flexGrow: 1 }}>
@@ -31,21 +49,12 @@ export default function App() {
             <IconButton edge="start" color="inherit" aria-label="menu">
               <MenuIcon />
             </IconButton>
+
             <Typography variant="h6" color="inherit" component="div" sx={{ flexGrow: 1 }}>
               Cody T.-H. Chiu
             </Typography>
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="linkedin"
-              href="https://www.linkedin.com/in/cody-t-h-c-647ab962"
-              target="_blank"
-            >
-              <LinkedInIcon />
-            </IconButton>
-            <IconButton edge="end" color="inherit" aria-label="github" href="https://github.com/codyit" target="_blank">
-              <GitHubIcon />
-            </IconButton>
+
+            <SocialIcons />
           </Toolbar>
         </AppBar>
       </Box>
